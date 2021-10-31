788d2480828cecd9d0c325d026598236f3b0c413
Caleb Mathew <creatrixity@gmail.com>
2021-10-31 20:25:39 +0100

    feat(characters-list): add column sorting

--- src/components/Table/BaseTable.js
+++ src/components/Table/BaseTable.js
@@ -8,17 +8,46 @@ import {
   Td,
   TableCaption,
 } from "@chakra-ui/react";
+import { useState } from "react";
 import { cm2Feet, cm2Inches } from "../../utils";
 
 const BaseTable = ({ data, schema }) => {
+  const [sortByColumn, setSortByColumn] = useState();
+  const [sortOrder, setSortOrder] = useState();
   const totalHeight = data.reduce((acc, curr) => acc + Number(curr.height), 0);
+
+  const sortColumn = (column) => {
+    if (sortByColumn) {
+      if (sortOrder === "desc") {
+        setSortOrder("asc");
+      } else {
+        setSortByColumn(null);
+      }
+    } else {
+      setSortByColumn(column);
+      setSortOrder("desc");
+    }
+  };
+
   return (
     <Table variant="simple">
       <TableCaption>All characters in this episode</TableCaption>
       <Thead>
         <Tr>
           {schema.map(({ name, accessor }) => (
-            <Th key={accessor}>{name}</Th>
+            <Th
+              key={accessor}
+              isNumeric={accessor === "height"}
+              onClick={() => sortColumn(accessor)}
+              style={{ cursor: "pointer" }}
+            >
+              {name}
+              {sortByColumn === accessor
+                ? sortOrder === "asc"
+                  ? " 🔽"
+                  : " 🔼"
+                : null}
+            </Th>
           ))}
         </Tr>
       </Thead>
@@ -29,11 +58,11 @@ const BaseTable = ({ data, schema }) => {
               <b>{item.name}</b>
             </Td>
             <Td>{item.gender}</Td>
-            <Td>{item.height} cm</Td>
+            <Td isNumeric>{item.height} cm</Td>
           </Tr>
         ))}
         <Tr>
-          <Td>feet</Td>
+          <Td>Stats</Td>
           <Td>{data.length}</Td>
           <Td isNumeric>
             {totalHeight} cm ({cm2Feet(totalHeight).toFixed(2)} ft /{" "}
@@ -43,7 +72,7 @@ const BaseTable = ({ data, schema }) => {
       </Tbody>
       <Tfoot>
         <Tr>
-          <Th>To convert</Th>
+          <Th>For nerds</Th>
           <Th>Total number</Th>
           <Th isNumeric>Total height</Th>
         </Tr>
